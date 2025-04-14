602025933698f7267aee37f1f1345b0f78b80717
Rosario Molina <rosariomolina6463@gmail.com>
2025-04-13 21:25:52 -0300

    ‘inicial’

---
+++ tp2_1_1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 20
+
+
+
+// codigo a completar
+int main()
+{
+
+    srand(time(NULL));
+    int i;
+    double vt[N];
+
+    for(i = 0; i<N; i++)
+    {
+    vt[i]= 1+ rand() %100; 
+    }
+    
+    for (i = 0; i < N; i++) {
+        printf("%f ", vt[i]);
+    }
+    
+    return 0;
+}
+
+
+
